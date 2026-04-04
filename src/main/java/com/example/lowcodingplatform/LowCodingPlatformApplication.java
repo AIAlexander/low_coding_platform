91c5930b517c9284699ead1106e24410cdf824c9
alex <alexander_wangwork@163.com>
2026-04-04 15:43:55 +0800

    init

---
+++ src/main/java/com/example/lowcodingplatform/LowCodingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.lowcodingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowCodingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LowCodingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lowcodingplatform/LowCodingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lowcodingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowCodingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
